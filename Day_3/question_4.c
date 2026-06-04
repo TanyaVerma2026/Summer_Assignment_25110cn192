6472c448fb1053ca0e188f2c8bc930c06d1e4939
TanyaVerma2026 <vermatanya1508@gmail.com>
2026-06-04 09:28:25 +0530

    Update question_4.c

--- Day_3/question_4.c
+++ Day_3/question_4.c
@@ -1,2 +1,23 @@
 //Write a program to Find LCM of two numbers. 
 
+#include <stdio.h>
+
+// Recursive function to find GCD
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    int a, b, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    lcm = (a * b) / gcd(a, b);
+
+    printf("LCM = %d", lcm);
+
+    return 0;
+}
