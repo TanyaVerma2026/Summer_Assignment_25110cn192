983bd709f28d5cffd31a7ff18b8797081e78a55d
Tanya <tv2586559@gmail.com>
2026-06-04 20:43:55 +0530

    update day_4

--- Day_4/question_2.c
+++ Day_4/question_2.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
-int main()
-{
-    int n,a = 0, b = 1, c, i;
+int main() {
+    int n, i, a = 0, b = 1, c;
 
     printf("Enter n: ");
     scanf("%d", &n);
 
-  
-        printf("The %dth Fibonacci term is %d", n, b);
-    
+    if (n < 0) {
+        printf("Invalid input!Please enter a non-negative integer.");
+    }
+    else if (n == 0) {
+        printf("Fibonacci term = %d", a);
+    }
+    else if (n == 1) {
+        printf("Fibonacci term = %d", b);
+    }
+    else {
+        for (i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        printf("Fibonacci term = %d", b);
+    }
+
     return 0;
 }
--- Day_4/question_4.c
+++ Day_4/question_4.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main() {
+    int start, end, num, temp, digits, rem;
+    int sum;
+
+    printf("Enter range: ");
+    scanf("%d %d", &start, &end);
+
+    printf("Armstrong numbers between %d and %d are:\n", start, end);
+
+    for (num = start; num <= end; num++) {
+        temp = num;
+        digits = 0;
+
+        while (temp != 0) {
+            digits++;
+            temp= temp/10;
+        }
+
+        temp = num;
+        sum = 0;
+
+        while (temp != 0) {
+            rem = temp % 10;
+            sum += pow(rem, digits);
+            temp= temp/10;
+        }
+
+        if (sum == num)
+            printf("%d ", num);
+    }
+
     return 0;
-    
 }
