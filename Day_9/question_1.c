b5b9c4c1be11c3cea2bb27777d373979694d937a
Tanya <tv2586559@gmail.com>
2026-06-09 11:55:02 +0530

    Day_9 uploaded

---
+++ Day_9/question_1.c
@@ -0,0 +1,17 @@
+//Write a program to Print reverse star pattern. 
+
+#include <stdio.h>
+int main(){
+    int input,i ,j;
+
+    printf("enter no. of rows:");
+    scanf("%d",&input);
+
+     for(i = input; i >= 1; i--) {
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
