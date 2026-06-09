bd7daea1db90b07a371951f74d91cd0cbb24d6dd
Tanya <tv2586559@gmail.com>
2026-06-09 11:59:40 +0530

    Day_9 uploaded

---
+++ Day_9/question_3.c
@@ -0,0 +1,19 @@
+//Write a program to Print repeated character pattern. 
+
+
+#include <stdio.h>
+int main(){
+    int input,i ,j;
+
+    printf("enter no. of rows:");
+    scanf("%d",&input);
+
+      for(i = 1; i <= input; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("%c", 'A' + i - 1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
